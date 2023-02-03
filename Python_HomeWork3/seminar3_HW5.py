7a82b1867b3f6cf5ebae89d1d757f05045ce6b67
GTrainee12 <113129604+GTrainee12@users.noreply.github.com>
2023-02-03 17:02:14 +0300

    Update seminar3_HW5.py

--- Python_HomeWork3/seminar3_HW5.py
+++ Python_HomeWork3/seminar3_HW5.py
@@ -1,21 +1,3 @@
-# n = int(input('Введите число: '))
-#
-# def get_fibonacci(n):
-#     f_n = []
-#     a, b = 1, 1
-#     for i in range(n-0):  # Номера фибоначи
-#         f_n.append(a)
-#         a, b = b, a + b
-#     a, b = 0, 1
-#     for i in range (n):    # Добавляем отрицательные номера
-#         f_n.insert(0, a)
-#         a, b = b, a - b
-#     return f_n
-#
-# f_n = get_fibonacci(n)
-# print(f"Список негафибоначчи: {get_fibonacci(n)}")
-# print(f"Индексы после 0: {f_n.index(0)}")
-
 n = int(input('Введите число: '))
 n_f = [1,-1]
 f = [1,1]
