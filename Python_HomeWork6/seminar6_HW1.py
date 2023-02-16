9a04ff15f2a50e0cccdc450cc89762bebcfa1697
Trainee <super.sel12@yadnex.ru>
2023-02-16 18:39:54 +0300

    HW6

--- Python_HomeWork6/seminar6_HW1.py
+++
@@ -1,8 +0,0 @@
-N = int(input('Введите длину массива: '))
-Array = [0]*N
-Array[0] = int(input('Введите первый элемен: '))
-difference = int(input('Введите разность: '))
-print(f"Ваш первый эллемет в массиве {Array[0]}")
-for i in range(1, N):
-    Array[i] = Array[i-1]+difference
-print(Array)
